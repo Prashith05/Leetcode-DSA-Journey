77df0b23ad6f99fe8a55e7e9d3d8a203b53457fc
Prashith kumar G <prashith3@gmail.com>
2026-07-07 22:28:04 +0530

    Time: 89 ms (5.66%), Space: 20.1 MB (94.35%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,11 +5,16 @@ class Solution(object):
         :rtype: bool
         """
         
-        seen = set()
-        for i in nums:
-            if i in seen:
-                return True
-            seen.add(i)
-        return False
+        # seen = set()
+        # for i in nums:
+        #     if i in seen:
+        #         return True
+        #     seen.add(i)
+        # return False
                       
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
         
