fa472ba55e6b1995566f7770f80a752e2b598d5f
Prashith kumar G <prashith3@gmail.com>
2026-09-03 17:12:10 +0530

    Time: 2 ms (89.12%), Space: 13.7 MB (39.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,16 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        count = 0
-        candidate = None
+        count = 0 
+        canditate = None
         for num in nums:
             if count == 0:
-                candidate = num
-
-            if num == candidate:
+                canditate = num
+            if canditate == num:
                 count += 1
             else:
                 count -= 1
+        return canditate
+        
 
-        return candidate
+
+
+
+        # count = 0
+        # candidate = None
+        # for num in nums:
+        #     if count == 0:
+        #         candidate = num
+
+        #     if num == candidate:
+        #         count += 1
+        #     else:
+        #         count -= 1
+
+        # return candidate
